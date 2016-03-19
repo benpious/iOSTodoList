2af16fcb7c49fab08ce6eb46458ccda6731bfb9c
benpious <benpious@gmail.com>
2016-03-19 11:02:27 -0700

    Initial Commit

---
+++ #Do/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  #Do
+//
+//  Created by Benjamin Pious on 3/19/16.
+//  Copyright © 2016 benpious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ #Do/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  #Do
+//
+//  Created by Benjamin Pious on 3/19/16.
+//  Copyright © 2016 benpious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
